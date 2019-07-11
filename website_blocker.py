afb2e419ac591431c56e7fd1067df2926bcb87b1
Chak Koppula <chak.koppula199@gmail.com>
2019-07-11 20:45:24 +0530

    minor changes

--- website_blocker.py
+++ website_blocker.py
@@ -1,14 +1,22 @@
 import time
 from datetime import datetime as dt
 
+# temporary file path for testing program
 hosts_temp = "hosts"
+# actual file path where file to be changed is located
 host_file_path = r"C:\Windows\System32\drivers\etc\hosts"
+# the address we will redirect the user to if they go to a blocked site, can be changed
 redirect = "127.0.0.1"
+# list of sites to block, has 2 variations per site as if only 1 address is used it doesn't work all the time
 block_site_list = ["www.facebook.com", "www.instagram.com", "www.reddit.com", "facebook.com", "instagram.com", "reddit.com"]
 
+# infinte loop keeps programming running until system is shut down
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 19):
-        print("working hours")
+    # checks if current time is between working hours (9 till 5)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+        #print("working hours")
+
+        # checks if site string from block_site_list is found in the file, if not it writes the redirect address and the websites address to the file
         with open(host_file_path, "r+") as file:
             file_content = file.read()
             for website in block_site_list:
@@ -18,14 +26,17 @@ while True:
                     file.write(redirect + " " + website + "\n")
 
     else:
-        print("fun hours")
+        #print("fun hours")
+
+        #checks each line of the file to see if it contains a website string, if it does then that line is not writen to the file
         with open(host_file_path, "r+") as file:
             file_content = file.readlines()
-            file.seek(0)
+            file.seek(0) #used to set cursor to start of file so we write at the top of the file
             for line in file_content:
                 if not any(website in line for website in block_site_list):
                     file.write(line)
                 else:
                     continue
-            file.truncate()
-    time.sleep(5)
+            file.truncate() # removes anything that comes after what we've just written to the top of the file
+                            # as we basicaly just re-wrote the file on top of the existing file
+    time.sleep(300) # waits five minutes as to not be a burden on CPU usage
--- website_blocker.pyw
+++ website_blocker.pyw
@@ -3,12 +3,12 @@ from datetime import datetime as dt
 
 # temporary file path for testing program
 hosts_temp = "hosts"
-# actual file path wher file to be changed is located
+# actual file path where file to be changed is located
 host_file_path = r"C:\Windows\System32\drivers\etc\hosts"
-# the address we will redirect the user to if they go to a blocked site
+# the address we will redirect the user to if they go to a blocked site, can be changed
 redirect = "127.0.0.1"
 # list of sites to block, has 2 variations per site as if only 1 address is used it doesn't work all the time
-block_site_list = ["www.facebook.com", "www.instagram.com", "facebook.com", "instagram.com"]
+block_site_list = ["www.facebook.com", "www.instagram.com", "www.reddit.com", "facebook.com", "instagram.com", "reddit.com"]
 
 # infinte loop keeps programming running until system is shut down
 while True:
@@ -16,7 +16,7 @@ while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
         #print("working hours")
 
-        # chekcs if site string from block_site_list is found in the file, if not it writes the redirect address and the websites address to the file
+        # checks if site string from block_site_list is found in the file, if not it writes the redirect address and the websites address to the file
         with open(host_file_path, "r+") as file:
             file_content = file.read()
             for website in block_site_list:
@@ -38,4 +38,5 @@ while True:
                 else:
                     continue
             file.truncate() # removes anything that comes after what we've just written to the top of the file
+                            # as we basicaly just re-wrote the file on top of the existing file
     time.sleep(300) # waits five minutes as to not be a burden on CPU usage
